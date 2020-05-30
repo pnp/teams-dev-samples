099f19325a6bd2f089154ad0d84e27f7a93c6b77
Bob German <bgerman1@hotmail.com>
2020-05-30 15:22:00 -0400

    Removed SSO sample for now

--- samples/tab-aad-sso/src/app/TeamsAppsComponents.ts
+++
@@ -1,4 +0,0 @@
-// Components will be added here
-export const nonce = {}; // Do not remove!
-// Automatically added for the ssoTab tab
-export * from "./ssoTab/SsoTab";
--- samples/tab-aad-sso/src/app/scripts/client.ts
+++
@@ -1,4 +0,0 @@
-// Automatically added for the ssoTab tab
-export * from "./ssoTab/SsoTab";
-export * from "./ssoTab/SsoTabConfig";
-export * from "./ssoTab/SsoTabRemove";
