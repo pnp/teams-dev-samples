910c986db5ce6da1432b6f7d8e272a53247a4d2c
Bob German <bgerman1@hotmail.com>
2020-08-18 22:20:45 -0400

    Tabs UI now gets the mail

--- samples/tab-aad-msal2/tab-aad-msal2/src/components/Tab.js
+++ samples/tab-aad-msal2/tab-aad-msal2/src/components/Tab.js
@@ -1,18 +1,32 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
 import React from 'react';
-import './App.css';
 import * as microsoftTeams from "@microsoft/teams-js";
 import TeamsAuthService from '../services/TeamsAuthService';
+import { List, Icon } from "@fluentui/react";
+import * as MicrosoftGraphClient from "@microsoft/microsoft-graph-client";
 
 class Tab extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            username: "",
-            token: ""
+            accessToken: null,
+            messages: []
         }
+    }
+
+    componentWillMount() {
+        this.msGraphClient = MicrosoftGraphClient.Client.init({
+            authProvider: async (done) => {
+                if (!this.state.accessToken) {
+                    const token = await TeamsAuthService
+                        .getAccessToken(["User.Read", "Mail.Read"],
+                            microsoftTeams);
+                    this.setState({
+                        accessToken: token
+                    });
+                }
+                done(null, this.state.accessToken);
+            }
+        });
     }
 
     render() {
@@ -21,19 +35,36 @@ class Tab extends React.Component {
             <div>
                 <button onClick={this.getMessages.bind(this)}>Get Mail</button>
                 <p>Username: {TeamsAuthService.getUsername()}</p>
-                <p>Access token: {this.state.token}</p>
-            </div>
+                <List selectable>
+                    {
+                        this.state.messages.map(message => (
+                            <List.Item media={<Icon name="email"></Icon>}
+                                header={message.receivedDateTime}
+                                content={message.subject}>
+                            </List.Item>
+                        ))
+                    }
+                </List>            </div>
         );
     }
 
     getMessages() {
-        TeamsAuthService.getAccessToken(["User.Read", "Mail.Read"], microsoftTeams)
-            .then((token) => {
-                this.setState({
-                    token: token
-                })
-            })
-            .catch((error) => { console.log(error); });
-    }
+
+        this.msGraphClient
+          .api("me/mailFolders/inbox/messages")
+          .select(["receivedDateTime", "subject"])
+          .top(15)
+          .get(async (error, rawMessages, rawResponse) => {
+            if (!error) {
+              this.setState(Object.assign({}, this.state, {
+                messages: rawMessages.value
+              }));
+            } else {
+              this.setState({
+                error: error
+              });
+            }
+          });
+      }
 }
 export default Tab;
