d32a400b300fa4791d602531504af5be82905abc
Bob German <bgerman1@hotmail.com>
2020-08-26 14:26:07 -0400

    Regenerate spfx sample

---
+++ samples/tab-aad-spfx/src/webparts/callGraphShowEmail/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICallGraphShowEmailWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CallGraphShowEmailWebPartStrings' {
+  const strings: ICallGraphShowEmailWebPartStrings;
+  export = strings;
+}
