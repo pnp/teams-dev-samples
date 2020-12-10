cd001df13e639aecb58cacaa61134471abec20e2
Sebastien Levert <slevert@outlook.com>
2020-12-10 16:43:22 -0500

    Fixing the Teams initialization when not in Teams

--- samples/tab-one-productivity-hub/src/components/mgt-theming.js
+++ samples/tab-one-productivity-hub/src/components/mgt-theming.js
@@ -1,10 +1,13 @@
-if(window.microsoftTeams) {
+if(window.microsoftTeams !== undefined) {
+  window.microsoftTeams.initialize();
   window.microsoftTeams.getContext(function(context) { 
-    document.body.classList.add(`mgt-${context.theme === "default" ? "light" : context.theme}`);
-  });
-  
-  window.microsoftTeams.registerOnThemeChangeHandler(function(theme) { 
-    document.body.classList = [""];
-    document.body.classList.add(`mgt-${theme === "default" ? "light" : theme}`);
+    if(context) {
+      document.body.classList.add(`mgt-${context.theme === "default" ? "light" : context.theme}`);
+      
+      window.microsoftTeams.registerOnThemeChangeHandler(function(theme) { 
+        document.body.classList = [""];
+        document.body.classList.add(`mgt-${theme === "default" ? "light" : theme}`);
+      });
+    }
   });
 }
