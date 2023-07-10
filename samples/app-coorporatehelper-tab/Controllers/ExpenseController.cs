90700401f40b9e666fb8415867cf93b442157429
Felipe Dotti <felipedotti@microsoft.com>
2023-07-10 18:32:36 +0100

    Commiting the sample Coorporate Helper - Issue #123 In hack-together-teams - felipedotti@microsoft.com - Commiting to teams-dev-samples for PR

---
+++ samples/app-coorporatehelper-tab/Controllers/ExpenseController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace CoorporateHelper.Controllers
+{
+
+	using System.Diagnostics;
+	using System.Net.Http;
+	using Microsoft.AspNetCore.Http;
+	using Microsoft.AspNetCore.Mvc;
+	using Microsoft.Extensions.Configuration;
+	using TeamsAuthSSO.Models;
+
+	public class ExpenseController : Controller
+	{
+
+		private readonly IConfiguration _configuration;
+		private readonly IHttpClientFactory _httpClientFactory;
+		private readonly IHttpContextAccessor _httpContextAccessor;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ExpenseController"/> class.
+		/// </summary>
+		/// <param name="configuration">IConfiguration instance.</param>
+		/// <param name="httpClientFactory">IHttpClientFactory instance.</param>
+		/// <param name="httpContextAccessor">IHttpContextAccessor instance.</param>
+		public ExpenseController(
+			IConfiguration configuration,
+			IHttpClientFactory httpClientFactory,
+			IHttpContextAccessor httpContextAccessor)
+		{
+			_configuration = configuration;
+			_httpClientFactory = httpClientFactory;
+			_httpContextAccessor = httpContextAccessor;
+		}
+
+		public IActionResult Expenses()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ samples/app-coorporatehelper-tab/Controllers/VacationController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+using System.Net.Http;
+using TeamsAuthSSO.Models;
+
+namespace CoorporateHelper.Controllers
+{
+	public class VacationController : Controller
+	{
+		private readonly IConfiguration _configuration;
+		private readonly IHttpClientFactory _httpClientFactory;
+		private readonly IHttpContextAccessor _httpContextAccessor;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="VacationController"/> class.
+		/// </summary>
+		/// <param name="configuration">IConfiguration instance.</param>
+		/// <param name="httpClientFactory">IHttpClientFactory instance.</param>
+		/// <param name="httpContextAccessor">IHttpContextAccessor instance.</param>
+		public VacationController(
+			IConfiguration configuration,
+			IHttpClientFactory httpClientFactory,
+			IHttpContextAccessor httpContextAccessor)
+		{
+			_configuration = configuration;
+			_httpClientFactory = httpClientFactory;
+			_httpContextAccessor = httpContextAccessor;
+		}
+
+		public IActionResult Vacations()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
