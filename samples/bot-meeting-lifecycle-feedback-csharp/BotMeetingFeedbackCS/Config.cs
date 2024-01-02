922bbbe75640e774e8be3f97035545ecd42a1b58
mmsharepoint <markus.moeller.ed@outlook.de>
2024-01-02 10:21:41 +0100

    New sample bot-meeting-lifecycle-feedback-csharp added

---
+++ samples/bot-meeting-lifecycle-feedback-csharp/BotMeetingFeedbackCS/Config.cs
@@ -0,0 +1,9 @@
+namespace BotMeetingFfeedbackCS
+{
+    public class ConfigOptions
+    {
+        public string BOT_ID { get; set; }
+        public string BOT_PASSWORD { get; set; }
+        public string BOT_ENDPOINT { get; set; }
+    }
+}
---
+++ samples/bot-meeting-lifecycle-feedback-csharp/BotMeetingFeedbackCS/Model/Feedback.cs
@@ -0,0 +1,13 @@
+﻿namespace BotMeetingFfeedbackCS.Model
+{
+    public class Feedback
+    {
+        public string meetingID { get; set; }
+        public string[] votedPersons { get; set; }
+        public int votes1 { get; set; }
+        public int votes2 { get; set; }
+        public int votes3 { get; set; }
+        public int votes4 { get; set; }
+        public int votes5 { get; set; }
+    }
+}
